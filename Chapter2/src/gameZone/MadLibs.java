3da6d2dfa824ed3c89d4aaba4bc29af001032623
mc307009 <michaelcurtis2421@gmail.com>
2017-09-05 09:28:59 -0500

    update Madlibs and others

---
+++ Chapter2/src/gameZone/MadLibs.java
@@ -0,0 +1,58 @@
+package gameZone;
+
+import java.util.Scanner;
+
+public class MadLibs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String color; 
+		String wordEst;
+		String bodyPartPlural;
+		String anAnimal;
+		String aNoun;
+		String PluralNoun;
+		
+		int a, b, c;
+		
+		c = a - b;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a color >>");
+		color = input.nextLine();
+		System.out.print("Enter a wordEst >>");
+		wordEst = input.nextLine();
+		System.out.print("Enter a bodyParPlural >>");
+		bodyPartPlural = input.nextLine();
+		System.out.print("Enter an Animal >>");
+		anAnimal = input.nextLine();
+		System.out.print("Enter aNoun");
+		aNoun = input.nextLine();
+		System.out.print("Enter a PluralNoun >>");
+		PluralNoun = input.nextLine();
+		System.out.print("Enter a number >> ");
+		a = input.nextInt();
+		System.out.print("Enter a number >>");
+		b = input.nextInt();
+		System.out.print("a - b = c");
+		c = a - b;
+		
+		
+		System.out.println("The color is " + color);
+		System.out.println("The wordEst is " + wordEst);
+		System.out.println("The bodyPartPlural is " + bodyPartPlural);
+		System.out.println("The Animal is " + anAnimal);
+		System.out.println("The Noun is " + aNoun);
+		System.out.print
+		
+		
+		
+		System.out.print("The " + color + " Dragon is the " + wordEst + "Dragon of all." 
+		
+		
+		
+
+	}
+
+}
