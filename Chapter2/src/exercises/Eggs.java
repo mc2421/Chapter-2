906ba37eb21ff4a57a8002a65dd43e566f3afd42
mc307009 <michaelcurtis2421@gmail.com>
2017-09-11 09:21:39 -0500

    update 2 9/11

--- Chapter2/src/exercises/Eggs.java
+++ Chapter2/src/exercises/Eggs.java
@@ -7,31 +7,24 @@ public class Eggs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Cost
-		double Egg = .045;
-		double DozenEggs = 3.25;
-		
-		int Eggs;
-		int Dozen = 12;
-		int ExtraEggs;
-		double Total;
 		Scanner input = new Scanner(System.in);
-		
-		Dozen = Eggs % 12;
-		
-		
-		System.out.println("Enter number of eggs");
+		int EggsInDozen = 12;
+		double EggCost = 0.45;
+		double DozenCost = 3.25;
+		int Eggs;
+		double Total;
+		System.out.println("Enter number of eggs >>> ");
 		Eggs = input.nextInt();
-		System.out.println("")
+		System.out.println("");
+		
+		int Dozen = Eggs / EggsInDozen;
+		int ExtraEggs;
 		
 		
-		
-		
-		
-		
-		
-		
-		System.out.println("You ordered" + Eggs + "eggs." + " That's" + Dozen + "at 3.25 per dozen and" + ExtraEggs +
-				" at 45 cents each for a total of");
+		ExtraEggs = Eggs % Dozen;
+		Total = (Dozen * DozenCost) + (ExtraEggs * EggCost);
+		System.out.println("You ordered " + Eggs + " eggs." + " That's " + Dozen + " at 3.25 per dozen and " + ExtraEggs +
+				" at 45 cents each for a total of " + Total); 
 	}
 
 }
